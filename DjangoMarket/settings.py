4728aca713fae8a372c0c238b105a6fd71efb656
User <asqarservis0000@gmail.com>
2025-07-16 18:30:38 +0300

    updated

--- DjangoMarket/settings.py
+++ DjangoMarket/settings.py
@@ -13,7 +13,7 @@ from os.path import join
 from pathlib import Path
 from os import getenv
 from dotenv import load_dotenv
-
+import redis
 Base_dir = Path(__file__).parent.parent
 load_dotenv(join(Base_dir, '.env'))
 
@@ -184,4 +184,14 @@ CACHES = {
 
 CELERY_BROKER_URL = getenv("REDIS_URL")
 
+import os
+import redis
+
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT")),
+    username=os.getenv("REDIS_USER"),
+    password=os.getenv("REDIS_PASS"),
+    decode_responses=True
+)
 
