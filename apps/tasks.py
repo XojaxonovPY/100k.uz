870cbf42b67d19581e82a0b5ea163285086d8bf9
dev <asqarservis00001@gmail.com>
2026-03-25 09:39:38 +0500

    deleted docker files and turn off send email function

--- apps/tasks.py
+++ apps/tasks.py
@@ -1,18 +1,18 @@
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 
-from root.settings import EMAIL_HOST_USER
+# from root.settings import EMAIL_HOST_USER
 
 
 # @shared_task
 def send_email(message: str, recipient_list: list[str]):
     try:
-        send_mail(
-            subject="Verification Code !!!",
-            message=message,
-            from_email=EMAIL_HOST_USER,
-            recipient_list=recipient_list,
-            fail_silently=False
-        )
+        # send_mail(
+        #     subject="Verification Code !!!",
+        #     message=message,
+        #     from_email=EMAIL_HOST_USER,
+        #     recipient_list=recipient_list,
+        #     fail_silently=False
+        # )
         return "✅ Email muvaffaqiyatli yuborildi."
     except Exception as e:
         return f"❌ Email yuborishda xatolik: {str(e)}"
